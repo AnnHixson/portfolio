4ea5bf61f3812e122497555b81861f0df6b35e6e
Ann <ann.hixson@uconn.edu>
2023-04-20 16:40:57 -0400

    improved first portfolio display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
+const kingDescription = document.getElementById('hidden-king');
+kingDescription.style.display = 'none';
 
+function displayKingDescription(event) {
+    event.preventDefault();
+
+    if (kingDescription.style.display === 'none') {
+        kingDescription.style.display = 'block';
+    } else if (kingDescription.style.display === 'block') {
+        kingDescription.style.display = 'none';
+    };    
+}
+
+document.getElementById('description-king').addEventListener('click', displayKingDescription);
